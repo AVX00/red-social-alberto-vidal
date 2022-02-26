0caa22f801d7a152fad6fdf3ac1e71a775484b49
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-26 22:16:34 +0100

    created context for user

---
+++ src/store/constexts/userContext/UserContextProvider.js
@@ -0,0 +1,12 @@
+import { useReducer } from "react";
+import userReducer from "../../reducers/userReducer";
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({ children }) => {
+  const [user, dispatch] = useReducer(userReducer, "");
+  return (
+    <UserContext.Provider value={(user, dispatch)}>
+      {children}
+    </UserContext.Provider>
+  );
+};
