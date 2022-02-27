f1d5e841b5ac7f9103df90715ab62606ebd86d81
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-27 10:52:30 +0100

    created login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Route } from "react-router-dom";
 import UserForm from "./components/UserForm";
+import Login from "./pages/Login";
 
 function App() {
-  return <UserForm isLogin={true} />;
+  return <Route path="/login" element={Login} />;
 }
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import UserForm from "../components/UserForm";
+
+const Login = () => {
+  return <UserForm isLogin={true} />;
+};
+
+export default Login;
