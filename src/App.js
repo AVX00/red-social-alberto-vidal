c3bcb6f11c3a1a392aec2eca88fd42bf437b40b0
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-27 11:12:11 +0100

    added styles to UserFormPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import UserForm from "./components/UserForm";
+import UserFormPage from "./pages/UserFormPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<UserForm isLogin={true} />} />
-      <Route path="/register" element={<UserForm isLogin={false} />} />
+      <Route path="/login" element={<UserFormPage isLogin={true} />} />
+      <Route path="/register" element={<UserFormPage isLogin={false} />} />
     </Routes>
   );
 }
--- src/pages/UserFormPage.js
+++ src/pages/UserFormPage.js
@@ -1,7 +1,25 @@
+import styled from "styled-components";
 import UserForm from "../components/UserForm";
 
+const FormContainer = styled.div`
+  margin: 0 auto;
+  border-radius: 5px;
+  background-color: #222;
+  height: 600px;
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  color: #f0f0f0;
+`;
+
 const UserFormPage = ({ isLogin }) => {
-  return <UserForm isLogin={isLogin} />;
+  return (
+    <FormContainer>
+      <UserForm isLogin={isLogin} />
+    </FormContainer>
+  );
 };
 
 export default UserFormPage;
