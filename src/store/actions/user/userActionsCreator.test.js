6dd3108c468913ec02d3fb592717ae887a3ea4ba
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-27 10:22:12 +0100

    tested action creator for logoutUser

--- src/store/actions/user/userActionsCreator.test.js
+++ src/store/actions/user/userActionsCreator.test.js
@@ -1,4 +1,7 @@
-import { loadUserActionCreator } from "./userActionsCreator";
+import {
+  loadUserActionCreator,
+  logOutUserActionCreator,
+} from "./userActionsCreator";
 import userActionTypes from "./userActionTypes";
 
 describe("Given a loadUserActionCreator", () => {
@@ -13,6 +16,20 @@ describe("Given a loadUserActionCreator", () => {
       };
 
       const action = loadUserActionCreator(user);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+describe("Given a logOutUserActionCreator", () => {
+  describe("When it's called", () => {
+    test("Then it should return an object with property type equal to logOutUser type", () => {
+      const expectedAction = {
+        type: userActionTypes.logOutUser,
+      };
+
+      const action = logOutUserActionCreator();
 
       expect(action).toEqual(expectedAction);
     });
