bee2a9b3d1996c0f0bc78660ceb1f1c3e06f0a31
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-01 12:20:03 +0100

    added users array and dispatch to users context

--- src/store/constexts/usersContext/UsersContextProvider.js
+++ src/store/constexts/usersContext/UsersContextProvider.js
@@ -1,7 +1,15 @@
+import { useReducer } from "react";
+import usersReducer from "../../reducers/usersReducer";
 import UsersContext from "./UsersContext";
 
 const UsersContextProvider = ({ children }) => {
-  return <UsersContext.Provider>{children}</UsersContext.Provider>;
+  const [users, dispatch] = useReducer(usersReducer, []);
+
+  return (
+    <UsersContext.Provider value={{ users, dispatch }}>
+      {children}
+    </UsersContext.Provider>
+  );
 };
 
 export default UsersContextProvider;
